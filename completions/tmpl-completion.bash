65705afd039a0f5272c0c6330a008862c2f5b2c4
Loic Coyle <loic.coyle@hotmail.fr>
2020-06-15 15:52:01 +0200

    moved variables to local

--- completions/tmpl-completion.bash
+++ completions/tmpl-completion.bash
@@ -1,25 +1,25 @@
 # Find the tmpl dir.
-[ -z $TMPL_DIR ] && TMPL_DIR=$XDG_TEMPLATES_DIR && [ -z $TMPL_DIR ] && TMPL_DIR=$HOME/Templates
 
 _tmpl () {
+    local tmpl_dir="$TMPL_DIR"
+    [ -z "$tmpl_dir" ] && tmpl_dir="$XDG_TEMPLATES_DIR" && [ -z "$tmpl_dir" ] && tmpl_dir="$HOME/Templates"
     case "${COMP_WORDS[1]}" in
         add )
             [ "${#COMP_WORDS[@]}" -eq 3 ] && COMPREPLY=( $(compgen -W "$(ls)" "${COMP_WORDS[-1]}") )
-            [ "${#COMP_WORDS[@]}" -eq 4 ] && COMPREPLY=( $(compgen -W "$(ls $TMPL_DIR)" "${COMP_WORDS[-1]}") )
+            [ "${#COMP_WORDS[@]}" -eq 4 ] && COMPREPLY=( $(compgen -W "$(ls $tmpl_dir)" "${COMP_WORDS[-1]}") )
             return
             ;;
         fetch )
-            [ "${#COMP_WORDS[@]}" -eq 3 ] && COMPREPLY=( $(compgen -W "$(ls $TMPL_DIR)" "${COMP_WORDS[-1]}") )
+            [ "${#COMP_WORDS[@]}" -eq 3 ] && COMPREPLY=( $(compgen -W "$(ls $tmpl_dir)" "${COMP_WORDS[-1]}") )
             [ "${#COMP_WORDS[@]}" -eq 4 ] && COMPREPLY=( $(compgen -W "$(ls)" "${COMP_WORDS[-1]}") )
             return
             ;;
         *)
             [ "${#COMP_WORDS[@]}" -eq 2 ] && COMPREPLY=( $(compgen -W "add fetch" "${COMP_WORDS[-1]}") )
-            [ "${#COMP_WORDS[@]}" -ge 3 ] && COMPREPLY=( $(compgen -W "$(ls $TMPL_DIR)" "${COMP_WORDS[-1]}") )
+            [ "${#COMP_WORDS[@]}" -ge 3 ] && COMPREPLY=( $(compgen -W "$(ls $tmpl_dir)" "${COMP_WORDS[-1]}") )
             return
             ;;
     esac
 }
 
 complete -F _tmpl tmpl
-
